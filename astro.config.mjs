12e93e409c70d1c7341c0ad86db45102294a5b60
Seraluce <evanyanet@gmail.com>
2026-07-06 02:10:50 +0000

    改为SSR模式，使用Cloudflare适配器

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://example.com',
-  output: 'static',
+  output: 'server',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
