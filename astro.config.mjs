79f6e89561dd1f005ed82dff580e72f252c4d914
seraluce <seraluce@163.com>
2026-07-07 06:15:08 +0800

    修复本地测试启动失败

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
- 
+
+const isDev = process.argv.includes('dev');
+
 export default defineConfig({
   site: 'https://example.com',
   output: 'server',
-  adapter: cloudflare({
+  adapter: isDev ? undefined : cloudflare({
     platformProxy: {
       enabled: process.env.USE_PLATFORM_PROXY === 'true',
     },
